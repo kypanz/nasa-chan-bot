168ab55fc8f563140691bdc60c94a6aa76c383c9
kypanz <kypanz95@gmail.com>
2024-08-22 00:49:33 -0300

    Fixing the linter of aws

--- src/aws/speech.ts
+++ src/aws/speech.ts
@@ -4,35 +4,39 @@ import logger from '../winston/configWinston';
 
 export async function saySomething(msg: string | null) {
 
-    const Polly = new AWS.Polly({
-        signatureVersion: 'v4',
-        region: 'us-east-1'
-    });
+  const Polly = new AWS.Polly({
+    signatureVersion: 'v4',
+    region: 'us-east-1'
+  });
 
-    const params = {
-        'Engine': 'neural',
-        'Text': msg || 'can you say me that again please ?, i have issues in my code',
-        'OutputFormat': 'mp3',
-        'VoiceId': 'Ivy'
-    }
+  const params = {
+    'Engine': 'neural',
+    'Text': msg || 'i have issues in my code',
+    'OutputFormat': 'mp3',
+    'VoiceId': 'Ivy'
+  }
 
-    await new Promise((resolve, reject) => {
-        Polly.synthesizeSpeech(params, (err : AWSError, data : AWS.Polly.Types.SynthesizeSpeechOutput) => {
-            if (err) {
-                console.log(err.code)
-                reject('error');
-                logger.error(err);
-            } else if (data) {
-                if (data.AudioStream instanceof Buffer) {
-                    fs.writeFile("./nasa-speak.mp3", data.AudioStream, function (err) {
-                        if (err) {
-                            return console.log(err)
-                        }
-                        console.log('write done');
-                        resolve('done');
-                    });
-                }
-            }
-        });
-    })
+  await new Promise((resolve, reject) => {
+    Polly.synthesizeSpeech(
+      params,
+      (
+        err: AWSError,
+        data: AWS.Polly.Types.SynthesizeSpeechOutput
+      ) => {
+        if (err) {
+          console.error(err.code)
+          reject('error');
+          logger.error(err);
+        } else if (data) {
+          if (data.AudioStream instanceof Buffer) {
+            fs.writeFile("./nasa-speak.mp3", data.AudioStream, function(err) {
+              if (err) {
+                return console.error(err)
+              }
+              resolve('done');
+            });
+          }
+        }
+      });
+  })
 }
